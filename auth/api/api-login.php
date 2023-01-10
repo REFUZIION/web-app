b7abc2373a8cc7008b09e28bec2f465cc9ce28c8
Diederik Veenstra <d.veenstra@estg.eu>
2023-01-10 17:23:22 +0100

    compared two hashed passwords instead of non hashed with hashed.

--- auth/api/api-login.php
+++ auth/api/api-login.php
@@ -8,9 +8,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username']) && isset(
     $stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
     $stmt->bind_param("ss", $username, $password);
 
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+    $pass_hash = password_hash($pass, PASSWORD_DEFAULT);
     // Set parameters and execute
-    $username = mysqli_real_escape_string($conn, $_POST['username']);
-    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $username = mysqli_real_escape_string($conn, $user);
+    $password = mysqli_real_escape_string($conn, $pass_hash);
     $stmt->execute();
     $result = $stmt->get_result();
     
