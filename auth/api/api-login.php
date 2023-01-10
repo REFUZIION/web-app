0301dffaf94c52c9b5e30f1c3ca745cf712a673b
Diederik Veenstra <d.veenstra@estg.eu>
2023-01-10 16:47:29 +0100

    Login handler file for C# app.

---
+++ auth/api/api-login.php
@@ -0,0 +1,26 @@
+<?php
+// Connect to database
+require_once('dbconfig.php');
+
+// Handle login request
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['username']) && isset($_POST['password'])) {
+    // Prepare and bind
+    $stmt = $conn->prepare("SELECT * FROM users WHERE username = ? AND password = ?");
+    $stmt->bind_param("ss", $username, $password);
+
+    // Set parameters and execute
+    $username = mysqli_real_escape_string($conn, $_POST['username']);
+    $password = mysqli_real_escape_string($conn, $_POST['password']);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    
+    if ($result->num_rows == 1) {
+        $user = $result->fetch_assoc();
+        // Login successful
+        echo "true";
+    } else {
+        // Login unsuccessful
+        echo "false";
+    }
+    $stmt->close();
+}
